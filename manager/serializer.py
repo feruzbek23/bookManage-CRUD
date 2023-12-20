cc6f78bc90c1f0f25800c37d1d415a6452da1dd8
unknown <feruzbek.dev@gmail.com>
2023-12-20 11:11:24 +0100

    Configured

--- manager/serializer.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import BooksModel
-
-class BooksSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = BooksModel
-        fields = '__all__'
